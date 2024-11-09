6691b48c036dcdfbec1f14c1bf7d2457fcd5a1d4
Rene <rene@karkkainen.net>
2024-11-09 20:29:45 +0200

    Camera

--- InventTek/src/app/camera/camera.component.ts
+++ InventTek/src/app/camera/camera.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {WebcamModule} from 'ngx-webcam';
+import { Subject, Observable } from 'rxjs';
+import { WebcamModule, WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 @Component({
   selector: 'app-camera',
@@ -10,5 +11,25 @@ import {WebcamModule} from 'ngx-webcam';
 })
 
 export class CameraComponent{
+  // Observable triggers for the snapshot and camera switch
+  private trigger: Subject<void> = new Subject<void>();
 
+  // Variable to store the captured image
+  public webcamImage: WebcamImage | null = null;
+
+  // Snapshot trigger as an observable
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  // Trigger snapshot
+  public takeSnapshot(): void {
+    this.trigger.next();
+  }
+
+  // Handle the captured image
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('Received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
 }
