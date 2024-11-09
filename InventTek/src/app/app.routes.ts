4429a46d25d5002ace8e7d8c4bdb016316b4968b
Kirill Sirotkin <kiriyms@gmail.com>
2024-11-09 20:26:42 +0200

    several components added

--- InventTek/src/app/app.routes.ts
+++ InventTek/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./title/title.component').then((m) => m.TitleComponent)
+    },
+},{
+    path: 'dashboard',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./dashboard/dashboard.component').then((m) => m.DashboardComponent)
+    },
+},{
+    path: 'equipment',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./equipment/equipment.component').then((m) => m.EquipmentComponent)
+    },
+},{
+    path: 'equipment-detail',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./equipment-detail/equipment-detail.component').then((m) => m.EquipmentDetailComponent)
+    },
+},{
+    path: 'camera',
+    pathMatch: 'full',
+    loadComponent: () => {
+        return import('./camera/camera.component').then((m) => m.CameraComponent)
+    },
+}];
