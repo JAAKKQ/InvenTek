8ae966244bd8de1df107861aa8441cdeb739be35
Rene <rene@karkkainen.net>
2024-11-09 18:16:42 +0200

    Webcam testing (not working)

--- InventTek/src/app/app.component.ts
+++ InventTek/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CameraComponent } from './camera/camera.component';
+import {WebcamModule} from 'ngx-webcam';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CameraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'InventTek';
 }
